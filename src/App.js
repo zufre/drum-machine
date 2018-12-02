ff7403d010eeddc79e4c7aca3270e6b0f98373f0
Cédric David <cedric.david@btopenworld.com>
2018-12-02 23:46:00 +1000

    add Drumpad

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Drumpad from "./Drumpad";
+const data = [
+  {
+    id: "snare",
+    letter: "Q",
+    src: "https://www.myinstants.com/media/sounds/snare.mp3"
+  },
+  {
+    id: "bass 1",
+    letter: "W",
+    src: "https://www.myinstants.com/media/sounds/bass-drum.mp3"
+  },
+  {
+    id: "bongo",
+    letter: "E",
+    src: "http://tipiwiki.free.fr/snd/Percussion(4e)2.wav"
+  },
+  {
+    id: "tom tom",
+    letter: "A",
+    src: "http://www.denhaku.com/r_box/sr16/sr16tom/loelectm.wav"
+  },
+  {
+    id: "bass 3",
+    letter: "S",
+    src: "http://billor.chsh.chc.edu.tw/sound/bass4.wav"
+  },
+  {
+    id: "shotgun",
+    letter: "D",
+    src: "http://david.guerrero.free.fr/Effects/ShotgunReload.wav"
+  },
+  {
+    id: "high hat",
+    letter: "Z",
+    src: "http://www.denhaku.com/r_box/tr707/closed.wav"
+  },
+  {
+    id: "drum hit",
+    letter: "X",
+    src: "http://www.masterbits.de/sc_docu/sounds1/1TM00013.MP3"
+  },
+  {
+    id: "laser",
+    letter: "C",
+    src: "http://www.sa-matra.net/sounds/starcontrol/Umgah-Backzip.wav"
+  }
+];
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div id="drum-machine">
+        <div id="display">
+          <Drumpad />
+        </div>
       </div>
     );
   }
---
+++ src/Drumpad.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default class Drumpad extends React.Component {
+  render() {
+    return <div className="drum-pad">Hi</div>;
+  }
+}
